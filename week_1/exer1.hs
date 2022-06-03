4c218e7dc0a690f70abfeaa5d10b1cd479e56304
Carabalone <guilherme.caraba@gmail.com>
2022-06-02 21:51:20 -0300

    credit card done

--- week_1/exer1.hs
+++ week_1/exer1.hs
@@ -1,9 +1,10 @@
 main :: IO()
 main = do
-  print (toDigits 123456789)
-  print (toDigitsRev 123456789)
-  print (doubleEveryOther (toDigitsRev 123456789))
-  print (sumDigits (toDigits 1234))
+  print (doubleEveryOtherLeft [8,7,6,5])
+  print (doubleEveryOtherLeft [1,2,3])
+  print (sumDigits(doubleEveryOther(toDigitsRev 4012888888881881)))
+  print (validate 4012888888881881)
+  print (validate 4012888888881882)
 
 toDigits :: Integer -> [Integer]
 toDigits x
@@ -14,15 +15,24 @@ toDigitsRev :: Integer -> [Integer]
 toDigitsRev x = reverse (toDigits x)
 
 doubleEveryOtherLeft :: [Integer] -> [Integer]
+doubleEveryOtherLeft [] = []
 doubleEveryOtherLeft (x:[]) = [x]
-doubleEveryOtherLeft (x:(h:t)) = x : 2*h : doubleEveryOther t
+doubleEveryOtherLeft (x:(h:t)) = x : 2*h : doubleEveryOtherLeft t
 
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther = reverse . doubleEveryOtherLeft . reverse
+doubleEveryOther = reverse . (doubleEveryOtherLeft . reverse)
 
 sumDigits :: [Integer] -> Integer
-sumDigits (h:[]) = h
-sumDigits (h:t) = h + sumDigits t
+sumDigits (h:[]) 
+  | h < 10 = h
+  | otherwise = h`div`10 + h`mod`10
+sumDigits (h:t) 
+  |h < 10 = h + sumDigits t
+  | otherwise = h `div` 10 + sumDigits ((h`mod`10):t)
 
 validate :: Integer -> Bool
-validate = sumDigits . doubleEveryOther . toDigits 
+validate x = ((sumDigits . (doubleEveryOther . toDigitsRev)) x) `mod`10 == 0
+
+
+
+
